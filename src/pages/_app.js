af85032155bea8d04b3698efa158bdbc10143051
marzieh <marziehmonshizade@yahoo.com>
2023-11-12 02:12:16 +0330

    add header,sidebar,footer in _app.js file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Footer from "../components/footer/footer";
+import Header from "../components/header/header";
+import Sidebar from "../components/sidebar/sidebar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="grid grid-cols-12 grid-flow-cols" dir="rtl">
+        <div className="col-span-1 h-screen sticky top-0 z-40">
+          <Sidebar />
+        </div>
+        <div className="col-span-11 bg-slate-100">
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
