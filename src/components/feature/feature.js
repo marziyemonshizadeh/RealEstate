78226943df6f86cb64f2773fba8a378e79597b5b
marzieh <marziehmonshizade@yahoo.com>
2023-11-17 18:33:58 +0330

    create feature component

---
+++ src/components/feature/feature.js
@@ -0,0 +1,14 @@
+export default function Feature({ title }) {
+  return (
+    <div className="hover:bg-gray-200 rounded-2xl p-4">
+      <h2 className="font-extrabold text-lg text-indigo-950">
+        بهترین خانه های لوکس جهان
+      </h2>
+      <p>
+        نکته بعدی در مورد متن ساختگی لورم ایپسوم این است که بعضی از طراحان
+        وبسایت و گرافیست کاران بعد از آنکه قالب و محتوای مورد نظرشون را ایجاد
+        کردند
+      </p>
+    </div>
+  );
+}
