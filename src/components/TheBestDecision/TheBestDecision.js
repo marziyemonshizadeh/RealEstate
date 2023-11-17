b434bc3c3bf91b80e05737cac2efdc9c996847e0
marzieh <marziehmonshizade@yahoo.com>
2023-11-17 18:34:29 +0330

    create TheBestDesition component

---
+++ src/components/TheBestDecision/TheBestDecision.js
@@ -0,0 +1,18 @@
+export default function TheBestDecision() {
+  return (
+    <div className="col-span-2 bg-slate-200 flex justify-center items-center flex-col leading-8">
+      <p>مشتریان خوشحال</p>
+      <h2 className="font-extrabold text-4xl text-indigo-950 mb-4">
+        ”بهترین تصمیم زندگی ما“
+      </h2>
+      <p className="max-w-lg">
+        متنی ساختگی و بدون معنی است که برای امتحان فونت و یا پر کردن فضا در یک
+        طراحی گرافیکی و یا صنعت چاپ استفاده میشود. طراحان وب و گرافیک از این متن
+        برای پرکردن
+      </p>
+      <button className="rounded-none bg-indigo-950 text-white p-2 my-2 hover:bg-indigo-900">
+        املاک ما را مشاهده کنید
+      </button>
+    </div>
+  );
+}
