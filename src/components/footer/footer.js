cec9138e086d888a179e611b8638a3f9ab867bd4
marzieh <marziehmonshizade@yahoo.com>
2023-11-18 20:45:37 +0330

    coding footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div className="bg-[#437FC7]">Footer</div>;
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-4 p-6 text-slate-200 text-center bg-indigo-950">
+      <p className="hover:text-indigo-400">خانه رویای خودراپیداکنید</p>
+      <p className="hover:text-indigo-400">درخواست پروپوزال</p>
+      <p className="hover:text-indigo-400">برنامه اجاره خانه</p>
+      <p className="hover:text-indigo-400">با ما تماس بگیرید</p>
+      <p className="hover:text-indigo-400">ملک خود راارسال کنید</p>
+      <p className="hover:text-indigo-400">باماکارکنید</p>
+    </div>
+  );
 }
