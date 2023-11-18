f57ad8f17e85330875d37d267222b997b7c97f0e
marzieh <marziehmonshizade@yahoo.com>
2023-11-18 23:58:10 +0330

    coding gallery component

---
+++ src/components/gallery/gallery.js
@@ -0,0 +1,103 @@
+export default function Gallery() {
+  return (
+    <div className="grid grid-cols-8 grid-rows-7 bg-slate-300 m-4 gap-2">
+      <div className="bg-slate-400 col-span-2 row-span-2">
+        {/* 1 */}
+        <img
+          src="https://96interior.com.sg/wp-content/uploads/2022/06/96-interior-modern-luxury-interior-design-2022-09.jpg"
+          alt="img"
+        />
+      </div>
+      <div className="bg-zinc-950 col-span-3 row-span-3">
+        {/* 2 */}
+        <img
+          src="https://hommes.studio/wp-content/uploads/2022/01/teletubby-style-modern-house-hidden-in-the-landscape-1.jpg"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-red-200 col-span-1 row-span-2">
+        {/* 3 */}
+        <img
+          src="https://www.offdecor.com/image/media/post/1603/mantel-decorating-ideas-wonderful-fireplace-decoration-ideas-uglysmoking-info-of-mantel-decorating-ideas.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-amber-400 col-span-2 row-span-2">
+        {/* 4 */}
+        <img
+          src="https://cdn.tatlerasia.com/tatlerasia/i/2022/10/31102544-prestige-global-14th-nov-20194868main_cover_1500x1001.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="col-span-2 row-span-3">
+        {/* 5 */}
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/541eaa20e4b0eb59dead1cd7/1691051617070-IGECW4MU27M2O99TVC0M/16+%28Phone%29.png?format=1000w"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-red-600 col-span-3 row-span-2">
+        {/* 6 */}
+        <img
+          src="https://www.verzun.com/cms-data/upload/luxury-real-estate-cote-d-azur-french-riviera-france-for-sale-2.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      {/* <div className="bg-teal-500 col-span-1 row-span-1">
+        7
+      </div> */}
+      <div className="bg-sky-600 col-span-2 row-span-2">
+        {/* 8 */}
+        <img
+          src="https://www.davidsmalldesigns.com/wp-content/uploads/2021/09/the-dusk-home-gallery-01-495x400.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-indigo-500 col-span-1 row-span-2">
+        {/* 9 */}
+        <img
+          src="https://cdn.tatlerasia.com/tatlerasia/i/2022/10/31111003-prestige-global-14th-nov-20199066_cover_1001x1500.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-red-400 col-span-3 row-span-2">
+        {/* 11 */}
+        <img
+          src="https://www.architectandinteriorsindia.com/cloud/2023/01/04/7.-Soft-Contoured-furniture-scaled.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-purple-500 col-span-3 row-span-3">
+        {/* 13 */}
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/610b30136c233b39bcf72dbd/1674845855482-V5MUSLBSX3K6NMWKUAQY/gatheraus-+white-houses-black+trim%231.jpeg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-fuchsia-600 col-span-2 row-span-3">
+        {/* 14 */}
+        <img
+          src="https://stylesatlife.com/wp-content/uploads/2023/05/Modern-Bungalow-Design.jpg.webp"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="bg-lime-700 col-span-3 row-span-2">
+        {/* 12 */}
+        <img
+          src="https://www.bellacollina.com/hubfs/Bella%20Collina_Montech_Lanai_1-a-1.jpg"
+          alt="img"
+          className="h-full w-full"
+        />
+      </div>
+    </div>
+  );
+}
