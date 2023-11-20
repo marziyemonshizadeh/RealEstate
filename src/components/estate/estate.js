f17c20294a1c3b54b69845ae5ef1392404d43cf4
marzieh <marziehmonshizade@yahoo.com>
2023-11-20 23:14:13 +0330

    refactor estate component

--- src/components/estate/estate.js
+++ src/components/estate/estate.js
@@ -1,4 +1,6 @@
-export default function Estate({ img }) {
+import Link from "next/link";
+
+export default function Estate({ id, img, title, roomCount, meterage, price }) {
   return (
     <div className="max-w-sm max-h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
@@ -7,16 +9,18 @@ export default function Estate({ img }) {
       <div className="p-5">
         <a href="#">
           <h5 className="text-2xl mb-3 font-bold tracking-tight text-gray-900 dark:text-white">
-            آپارتمان خانوادگی مدرن
+            {title}
           </h5>
         </a>
         <div className="mb-3 font-normal grid grid-cols-2 gap-2">
           <p>مالدیو</p>
-          <p>4 اتاق</p>
-          <p>400 متر مربع</p>
-          <p>560 میلیون تومان</p>
+          <p>{roomCount} اتاق</p>
+          <p>{meterage} متر مربع</p>
+          <p>{price} میلیون تومان</p>
         </div>
-        <button className="btn py-2">املاک ما را مشاهده کنید</button>
+        <Link href={`/home/${id}`} className="btn py-2">
+          املاک ما را مشاهده کنید
+        </Link>
       </div>
     </div>
   );
