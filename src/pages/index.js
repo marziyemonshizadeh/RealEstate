6f35b0b4532196b81a56204ff8cdd52f83345df5
marzieh <marziehmonshizade@yahoo.com>
2023-11-20 23:07:31 +0330

    refactor pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,26 @@ import TheBestDecision from "@/components/TheBestDecision/TheBestDecision";
 import Estate from "@/components/estate/estate";
 import Feature from "@/components/feature/feature";
 import Gallery from "@/components/gallery/gallery";
+import { useEffect, useState } from "react";
+
 function index() {
+  const [estate, setEstate] = useState([]);
+  const [feature, setFeature] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4001/Estates")
+      .then((res) => res.json())
+      .then((estate) => setEstate(estate));
+    fetch("http://localhost:4001/Feature")
+      .then((res) => res.json())
+      .then((feature) => setFeature(feature));
+  }, []);
   return (
     <>
       {/* ویژگی ها */}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 my-16 mx-4">
-        <Feature title=" بهترین خانه های لوکس جهان" />
-        <Feature title="همه خانه ها در مکان های برتر " />
-        <Feature title="خانه جدید در یک هفته" />
-        <Feature title="فقط بهترین خواص" />
-        <Feature title="پرداخت های ایمن در بعدی" />
-        <Feature title=" 1% مشاوران املاک برتر" />
+        {feature.map((feature) => {
+          return <Feature key={feature.id} {...feature} />;
+        })}
       </div>
       {/* تعریف :) */}
       <div className="grid xl:grid-cols-4 md:grid-cols-4 grid-cols-1  grid-flow-cols">
@@ -27,12 +36,9 @@ function index() {
       </div>
       {/* املاک */}
       <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 place-items-center gap-y-8 my-16">
-        <Estate img="https://www.decorilla.com/online-decorating/wp-content/uploads/2022/03/Minimalist-modern-home-interior-design-with-a-patio.jpeg" />
-        <Estate img="https://www.decorilla.com/online-decorating/wp-content/uploads/2022/03/Minimalist-modern-home-interior-design-with-a-patio.jpeg" />
-        <Estate img="https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1627923147715-B3RH11DQ5LOYJ4DMNDSS/Bundy+Drive+Brentwood+spacious+modern+open+plan+mansion+interior+design" />
-        <Estate img="https://www.eligasht.com/Blog/wp-content/uploads/2022/08/Coastal-cities-world.jpg" />
-        <Estate img="https://img.freepik.com/premium-photo/real-estate-dream-home_839035-11884.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699488000&semt=ais" />
-        <Estate img="https://www.decorilla.com/online-decorating/wp-content/uploads/2022/03/Minimalist-modern-home-interior-design-with-a-patio.jpeg" />
+        {estate.map((estate) => {
+          return <Estate key={estate.id} {...estate} />;
+        })}
       </div>
       <Gallery />
     </>
