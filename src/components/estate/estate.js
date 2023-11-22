bf3a1eb08fab64b460fe903d3ac17e02a06f0424
marzieh <marziehmonshizade@yahoo.com>
2023-11-22 22:13:18 +0330

    add Link

--- src/components/estate/estate.js
+++ src/components/estate/estate.js
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Estate({ id, img, title, roomCount, meterage, price }) {
   return (
-    <div className="max-w-sm max-h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
+    <div className="max-w-[25rem] max-h-[40rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <Link href="#">
         <img className="rounded-t-lg h-72 w-full" src={img} alt="img" />
-      </a>
+      </Link>
       <div className="p-5">
-        <a href="#">
+        <Link href="#">
           <h5 className="text-2xl mb-3 font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
-        </a>
-        <div className="mb-3 font-normal grid grid-cols-2 gap-2">
+        </Link>
+        <div className="font-normal grid grid-cols-2 gap-2 mb-5">
           <p>مالدیو</p>
           <p>{roomCount} اتاق</p>
           <p>{meterage} متر مربع</p>
