1c349f85ea04c9c71e2168c36b4322840b921934
marzieh <marziehmonshizade@yahoo.com>
2023-11-23 16:36:44 +0330

    add description on feature component

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -1,14 +1,8 @@
-export default function Feature({ title }) {
+export default function Feature({ title, description }) {
   return (
     <div className="hover:bg-gray-200 rounded-2xl p-4">
-      <h2 className="font-extrabold text-lg text-indigo-950">
-        بهترین خانه های لوکس جهان
-      </h2>
-      <p>
-        نکته بعدی در مورد متن ساختگی لورم ایپسوم این است که بعضی از طراحان
-        وبسایت و گرافیست کاران بعد از آنکه قالب و محتوای مورد نظرشون را ایجاد
-        کردند
-      </p>
+      <h2 className="font-extrabold text-lg text-indigo-950">{title}</h2>
+      <p>{description}</p>
     </div>
   );
 }
