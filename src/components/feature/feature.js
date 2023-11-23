1c3e33da8ff8c2a2d7187c5dfd3bd8538133d3ed
marzieh <marziehmonshizade@yahoo.com>
2023-11-24 02:14:37 +0330

    Add children in feature component

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -1,8 +1,10 @@
-export default function Feature({ title, description }) {
+export default function Feature({ title, children }) {
   return (
-    <div className="hover:bg-gray-200 rounded-2xl p-4">
-      <h2 className="font-extrabold text-lg text-indigo-950">{title}</h2>
-      <p>{description}</p>
+    <div className="hover:bg-gray-200 text-center  rounded-2xl p-4">
+      <h2 className="font-extrabold text-lg hover:text-xl text-indigo-950">
+        {title}
+      </h2>
+      <p>{children}</p>
     </div>
   );
 }
